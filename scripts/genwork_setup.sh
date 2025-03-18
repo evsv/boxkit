cea59164c19997c3fc112d188f50add64f872138
vishwanathevs <vishwanathevs@gmail.com>
2025-03-17 21:05:53 -0400

    Allowing apt to automatically answer yes to confirmations

--- scripts/genwork_setup.sh
+++ scripts/genwork_setup.sh
@@ -5,7 +5,7 @@
 
 # Update the container and install packages
 apt update && apt upgrade
-grep -v '^#' ./genwork.packages | xargs apt install
+grep -v '^#' ./genwork.packages | xargs apt -y install
 
 # Installing python libraries
 pip install pandas numpy ollama bertopic gensim
@@ -16,5 +16,5 @@ Rscript -e "install.packages(c(\"readr\", \"dplyr\", \"tidyr\", \"kableExtra\", 
 # Installing Ollama and other deb files not present in repos
 wget https://github.com/quarto-dev/quarto-cli/releases/download/v1.6.42/quarto-1.6.42-linux-amd64.deb
 
-apt install ./quarto-1.6.42-linux-amd64.deb && rm ./quarto-1.6.42-linux-amd64.deb
+apt -y install ./quarto-1.6.42-linux-amd64.deb && rm ./quarto-1.6.42-linux-amd64.deb
 curl -fsSL https://ollama.com/install.sh | sh
