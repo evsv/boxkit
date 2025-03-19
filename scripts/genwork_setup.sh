f8bfb15880c1cc19d5a4b3af10c7433d51efb7ca
vishwanathevs <vishwanathevs@gmail.com>
2025-03-19 17:02:14 -0400

    Cleaning up

--- scripts/genwork_setup.sh
+++ scripts/genwork_setup.sh
@@ -7,13 +7,6 @@
 apt update && apt upgrade
 grep -v '^#' ./genwork.packages | xargs apt -y install
 
-# Installing python libraries
-# python -m venv /general
-# /general/bin/pip install -r requirements.txt
-
-# Installing R libraries
-#Rscript -e "install.packages(c(\"readr\", \"dplyr\", \"tidyr\", \"kableExtra\", \"ggplot2\", \"stringr\", \"lubridate\", \"igraph\"), lib=\"~/R/libs\")"
-
 # Installing Quarto and other debs not present in the repos
 wget https://github.com/quarto-dev/quarto-cli/releases/download/v1.6.42/quarto-1.6.42-linux-amd64.deb
 apt -y install ./quarto-1.6.42-linux-amd64.deb && rm ./quarto-1.6.42-linux-amd64.deb
