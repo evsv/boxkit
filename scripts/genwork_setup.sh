e7cb253eb7d9d09822341d148b681f54562449ce
Vishwanath E.V.S. <vishwanathevs@gmail.com>
2025-04-29 19:26:18 -0400

    Moving package installations into script from containerfile

--- scripts/genwork_setup.sh
+++ scripts/genwork_setup.sh
@@ -11,3 +11,15 @@ grep -v '^#' ./genwork.packages | xargs apt -y install
 wget https://github.com/quarto-dev/quarto-cli/releases/download/v1.6.42/quarto-1.6.42-linux-amd64.deb
 apt -y install ./quarto-1.6.42-linux-amd64.deb && rm ./quarto-1.6.42-linux-amd64.deb
 
+# Installing R and Python Packages
+R -e "install.packages('readr', dependencies=TRUE, repos='http://cran.rstudio.com/')" && \
+    R -e "install.packages('tidyr', dependencies=TRUE, repos='http://cran.rstudio.com/')" && \
+    R -e "install.packages('dplyr', dependencies=TRUE, repos='http://cran.rstudio.com/')" && \
+    R -e "install.packages('ggplot2', dependencies=TRUE, repos='http://cran.rstudio.com/')" && \
+    R -e "install.packages('knitr', dependencies=TRUE, repos='http://cran.rstudio.com/')" && \
+    R -e "install.packages('kableExtra', dependencies=TRUE, repos='http://cran.rstudio.com/')" && \
+    R -e "install.packages('igraph', dependencies=TRUE, repos='http://cran.rstudio.com/')"
+
+RUN python3 -m venv /pyenv && \
+    /pyenv/bin/pip install -r requirements.txt && \
+    chmod 777 -R /pyenv && rm /requirements.txt
